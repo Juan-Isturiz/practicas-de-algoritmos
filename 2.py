ba3ca2cd5ed969c769ea9e6f0f786e5ad7890c53
Juan Isturiz <juan.isturiz@correo.unimet.edu.ve>
2020-10-02 17:48:27 -0400

    arreglada funcion 'sofa'

--- 2.py
+++ 2.py
@@ -2,24 +2,38 @@
 def convert(temp,unit):
     if temp.isnumeric():
         temp = float(temp)
- 
-        if unit.lower().strip(' ') == "k":
-         
+        if unit.lower().strip(' ') == "k": 
             temp -= 273.15
             print(f'\nThe Temperature is {temp}C°')
-            
-        
         elif unit.lower().strip(' ') == "f":
-    
             temp = (temp - 32) * 5/9
-            print(f'\nThe Temperature is {ftemp}C°')
-            
-        
+            print(f'\nThe Temperature is {ftemp}C°')    
     else:
         print ('\nWrong input, try again')
 
-t = input('\nWich is the room temp?: ') 
-u = input('\nWich is the temp unit (K or F)?: ')
+#t = input('\nWich is the room temp?: ') 
+#u = input('\nWich is the temp unit (K or F)?: ')
+#convert(t,u)
+import datetime
+a = datetime.date.today().year
+def sofa(name, lastna, age):
+    nn = []
+    ll = []
+    for letter in name:
+        nn.append(letter)
+    for letter in lastna:
+        ll.append(letter)
+    lena = len(nn)
+    lela = len(ll)
+    print(f'your name is {lena} characters long')
+    print(f'your name is {lela} characters long') 
+    year = a - int(age)
+    year2 = a - int(age)  - 1    
+    print (f'You were born in {year} or {year2}')
 
-convert(t,u)
-            
+
+#name = input('What is your name?: ')
+#lastname = input('What is your lastname?: ')
+#age = input('How old are you?: ')
+#
+#sofa (name, lastname, age)
